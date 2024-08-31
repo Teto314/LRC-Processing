821c45d7d46a1aa7b161da03fb9e57b547adce5f
Richard He <150879164+Teto314@users.noreply.github.com>
2024-08-31 23:24:23 +0800

    def parse_lrc_content bug fix

--- LRC→ASS/LRC-ASS.py
+++ LRC→ASS/LRC-ASS.py
@@ -63,12 +63,7 @@ def parse_lrc_content(lrc_content_input):
     i = 0
     for line in lines:
         line = line.strip()  # 去除行首尾的空白字符
-        if line.startswith('[') and line.endswith(']') and i < 4 and len(line) > 6:  # 我默认你只有4行注释，且首行字幕不是空行
-            if line[3] != ':' or line[6] != '.':
-                key, value = line[1:-1].split(':', 1)
-                metadata_main[key.strip()] = value.strip()
-                i += 1 
-        elif line.startswith('[') and line.endswith(']') and i < 4 and len(line) <= 6:
+        if line.startswith('[') and line.endswith(']') and i < 4:  # 我默认你只有4行注释，且首行字幕不是空行
             key, value = line[1:-1].split(':', 1)
             metadata_main[key.strip()] = value.strip()
             i += 1
